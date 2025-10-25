842e0c19fd3e2bcfdb074480e15797b5fe4bc0cd
Malak Abdelhamed <marakuoo82@gmail.com>
2025-10-25 18:53:58 +0300

    Update Params.cs

--- MethodParameters/Params.cs
+++ MethodParameters/Params.cs
@@ -40,6 +40,50 @@ namespace malak
 
 
     
+// email subjet from to 
+using System;
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text; // for string builder 
+
+
+
+namespace malak
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateEmail("Meeting", "boss@company.com", "alice@company.com", "bob@company.com");
+            CreateEmail("Newsletter", "news@company.com", "subscriber1@email.com");
+
+
+        }
+
+        static void CreateEmail(string subject, string from, params string[] recipients)
+        {
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"From: {from}");
+            Console.WriteLine("To: " + string.Join(", ", recipients));
+        }
+
+
+
+
+    }
+    }
+
+
+    
+
+
+
 
 
 
