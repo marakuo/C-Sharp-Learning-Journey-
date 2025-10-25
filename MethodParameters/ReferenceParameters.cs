bf8b02fa06a71a7ad2fe79b7ee07543078974a60
Malak Abdelhamed <marakuoo82@gmail.com>
2025-10-25 14:52:18 +0300

    Update ReferenceParameters.cs

--- MethodParameters/ReferenceParameters.cs
+++ MethodParameters/ReferenceParameters.cs
@@ -1,3 +1,4 @@
+// change old password 
 using System;
 using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
@@ -41,6 +42,87 @@ namespace malak
 
 
     }
+// reset to zero 
+using System;
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text; // for string builder 
+
+
+
+namespace malak
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int hours = 4;
+            int minutes = 40;
+            Console.WriteLine($"Time Taken {hours} : {minutes}");
+            ResetToZero(ref hours);
+            ResetToZero(ref minutes);
+            Console.WriteLine($"Time reset {hours} : {minutes}");
+
+        }
+        static void ResetToZero(ref int reset)
+        {
+            reset = 0;
+            
+        }
+    
+    }
+
+
+    }
+
+// capitalize string 
+using System;
+using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text; // for string builder 
+
+
+
+namespace malak
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the name: ");
+            string name = Console.ReadLine();
+
+            Capitalize(ref name);
+            Console.WriteLine(name);
+
+
+        }
+        static void Capitalize(ref string name)
+        {
+            name = name.ToUpper();
+            
+
+        }
+    
+    }
+    }
+
+
+
+
+
 
 
 
