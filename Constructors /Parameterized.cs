1f05fe41a057dde3c754664992930ee9b3c59539
Malak Abdelhamed <marakuoo82@gmail.com>
2025-11-20 17:36:09 +0200

    Update Parameterized.cs

--- Constructors /Parameterized.cs
+++ Constructors /Parameterized.cs
@@ -30,7 +30,7 @@ namespace csharpcode
     }
     
 }
-// multiple 
+// Overloading
 using System;
 using System.Reflection;
 
@@ -74,4 +74,81 @@ namespace csharpcode
         }
     }
     
+}
+// using parametrized constructor for static fields
+using System;
+using System.Reflection;
+
+namespace csharpcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car1 = new Car("BMW");
+            Car car2 = new Car("Audi");
+            Car car3 = new Car("BMW");
+            // count the number of cars created for the whole class
+            Console.WriteLine("The number of cars "+Car.CarCounter); // 3
+             
+
+        }
+    }
+
+    class Car
+    {
+        public string brand;
+        public static int CarCounter;
+
+        public Car(string brand)
+        {
+            this.brand = brand;
+            CarCounter++;
+           
+           
+        }
+        
+
+    }
+}
+// ex 2 
+using System;
+using System.Reflection;
+
+namespace csharpcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassMeeting[] students =
+            {
+                new ClassMeeting("Malk", "njhfgmail.com", 67643),
+                new ClassMeeting("Heba", "heb@gmail.com", 67364)
+            };
+            Console.WriteLine("The number of students in the meeting is " +ClassMeeting.studentCounter);
+
+
+
+        }
+    }
+
+    class ClassMeeting{
+        public string studentName;
+        public string studentEmail;
+        public int studentId;
+        public static int studentCounter;
+
+        public ClassMeeting(string studentName, string studentEmail, int studentId)
+        {
+            this.studentName = studentName;
+            this.studentEmail = studentEmail;
+            this.studentId = studentId;
+            studentCounter++;
+        }
+        
+
+
+    }
 }
